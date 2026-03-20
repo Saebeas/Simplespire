7269f745a62eac3a77cc7d75ec163c74de0c5147
Saebeas <stephenanthonyearl@gmail.com>
2026-03-20 16:31:59 -0400

    card counters

--- ui/card_hand_ui.gd
+++ ui/card_hand_ui.gd
@@ -5,7 +5,8 @@ extends HBoxContainer
 const SLOT_COUNT: int = 4
 
 var _slots: Array = []  # Array of CardSlotUI nodes
-
+@onready var deck_counter: Label = $DeckCounter
+@onready var discard_counter: Label = $DiscardCounter
 
 func _ready() -> void:
 	var slots := []
@@ -22,6 +23,11 @@ func _ready() -> void:
 	EventBus.summon_reticle_cancelled.connect(_on_reticle_cancelled)
 	EventBus.insufficient_resources.connect(_on_insufficient_resources)
 
+func _update_counters() -> void:
+	var total: int = CardManager.deck.size() + CardManager.discard.size() + CardManager.get_hand().reduce(func(count, card): return count + (1 if card != null else 0), 0)
+	deck_counter.text = "Deck\n%d/%d" % [CardManager.deck.size(), total]
+	discard_counter.text = "Discard\n%d/%d" % [CardManager.discard.size(), total]
+
 func setup_slots(slots: Array) -> void:
 	_slots = slots
 	_refresh_all()
@@ -32,13 +38,14 @@ func _refresh_all() -> void:
 	for i in range(_slots.size()):
 		_slots[i].set_card(hand[i])
 		_slots[i].set_active(false)
-
+	_update_counters()
 
 func _on_card_drawn(_card: Resource, slot: int) -> void:
 	if slot >= _slots.size():
 		return
 	_slots[slot].set_card(_card)
 	_slots[slot].set_active(false)
+	_update_counters()
 
 
 func _on_card_played(_card: Resource, slot: int, _mode: int) -> void:
@@ -46,6 +53,7 @@ func _on_card_played(_card: Resource, slot: int, _mode: int) -> void:
 		return
 	_slots[slot].set_card(null)
 	_slots[slot].set_active(false)
+	_update_counters()
 
 
 func _on_reticle_activated(hand_index: int) -> void:
